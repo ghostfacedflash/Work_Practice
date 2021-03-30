0e77bd09e6e85753c30838b152b8bf274f356726
Matthew C. Smith <matthewcsmith@WWPROGRAMMING.COM>
2021-03-30 13:27:15 -0500

    Initial

---
+++ More_Practice/app.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove
+    }
+})
+
+app.mount('#app')
